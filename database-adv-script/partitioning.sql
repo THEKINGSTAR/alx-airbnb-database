d573dac6e9a5b09e27961f8467f6b2f82ad1307b
THEKINGSTAR <thekingstar5002@gmail.com>
2025-07-07 06:00:33 +0300

    5. Partitioning Large Tables

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,24 @@
+-- Drop old Booking table if exists (BE CAREFUL)
+DROP TABLE IF EXISTS Booking;
+
+-- Create partitioned Booking table on start_date
+CREATE TABLE Booking (
+    booking_id CHAR(36) NOT NULL,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (booking_id, start_date),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+    PARTITION p2022 VALUES LESS THAN (2023),
+    PARTITION p2023 VALUES LESS THAN (2024),
+    PARTITION p2024 VALUES LESS THAN (2025),
+    PARTITION p2025 VALUES LESS THAN (2026),
+    PARTITION pmax  VALUES LESS THAN MAXVALUE
+);
